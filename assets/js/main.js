2cd1898fcb3e9d9245703ab05c0226e27eb82b5d
K.V. <30656341+KVonGit@users.noreply.github.com>
2025-04-16 22:32:18 -0500

    Update style settings

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,46 @@
 const init = () => {
-    const loadPage = () => {
-        const loadingDiv = document.querySelector('#loading-div');
-        loadingDiv.style.display = 'none';
-        const contentDiv = document.querySelector('#content-div');
-        contentDiv.innerHTML = `
-            <h1>KVonGit</h1>
-            <h2>About Me</h2>
-            <p>Hi, I'm KV! I'm usually messing around with Quest code.</p>
-            <br/>
-            <h2>Links</h2>
-            <ul>
-            <li><a href="https://github.com/KVonGit/quest5-stuff">Quest 5 Stuff</a></li>
-            <li><a href="https://github.com/textadventures/quest">textadventures/quest</a></li>
-            <li><a href="http://docs.textadventures.co.uk/quest/">Quest Documentation</a></li>
-            <li><a href="http://docs.textadventures.co.uk/quest/tutorial/">Quest Tutorial</a></li>
-            <li><a href="https://github.com/ThePix/quest/wiki">Pixie's Wiki</a></li>
-            <li><a href="https://archive.textadventures.co.uk/forum">textadventures Forum Archive (2003-2024)</a></li>
-            <li><a href="https://www.axeuk.com/ubb/">Axe Forum Archive (2000-2003)</a></li>
-            <li><a href="https://discord.com/channels/1278365322054996018/1314620834467545200">#quest channel on Discord</a></li>
-            </ul>`;
-        contentDiv.style.display = 'block';
-    };
+    const loadingDiv = document.querySelector('#loading-div');
+    loadingDiv.style.display = 'none';
+    const contentDiv = document.querySelector('#content-div');
+    contentDiv.innerHTML = `
+        <h1>KVonGit</h1>
+        <h2>About Me</h2>
+        <p>Hi, I'm KV! I'm usually messing around with Quest code.</p><hr/>
+        <h2>Links</h2>
+        <table>
+          <tbody>
+            <tr>
+                <td><button onclick="window.open('https://github.com/KVonGit/quest5-stuff');">Quest 5 Stuff</button></td>
+            </tr>
+            <tr>
+                <td><button onclick="window.open('https://github.com/textadventures/quest');">textadventures/quest</button></td>
+            </tr>
+            <tr>
+                <td><button onclick="window.open('http://docs.textadventures.co.uk/quest/');">Quest Documentation</button></td>
+            </tr>
+            <tr>
+                <td><button onclick="window.open('http://docs.textadventures.co.uk/quest/tutorial/');">Quest Tutorial</button></td>
+            </tr>
+            <tr>
+                <td><button onclick="window.open('https://github.com/ThePix/quest/wiki');">Pixie's Wiki</button></td>
+            </tr>
+            <tr>
+                <td><button onclick="window.open('https://archive.textadventures.co.uk/forum');">textadventures Forum Archive (2003-2024)</button></td>
+            </tr>
+            <tr>
+                <td><button onclick="window.open('https://www.axeuk.com/ubb/');">Axe Forum Archive (2000-2003)</button></td>
+            </tr>
+            <tr>
+                <td><button onclick="window.open('https://discord.com/channels/1278365322054996018/1314620834467545200');">#quest channel on Discord</button> -- <button onclick="window.open('https://discord.gg/vME4fcdrEp');">Non-expiring invite</button></td>
+            </tr>
+        </tbody>
+    </table>`;
+    contentDiv.style.display = 'block';
+
     //const button = document.querySelector('.button');
     //button.addEventListener('click', (event) => {
         //handleClick(event);
     //});
-    
-    loadPage();
 };
 
 (() => {
